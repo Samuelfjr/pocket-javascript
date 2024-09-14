5a590f46aae7182a75a70b3e1b8367e43eb26786
Samuelfjr <devsamuelfjr@gmail.com>
2024-09-14 11:38:27 -0300

    feat: creates a route that returns the outstanding goals for the week

--- src/http/server.ts
+++ src/http/server.ts
@@ -6,11 +6,18 @@ import {
 } from 'fastify-type-provider-zod'
 import { createGoal } from '../functions/create-goal'
 import z from 'zod'
+import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
+
+app.get('/pending-goals', async () => {
+  const { pendingGoals } = await getWeekPendingGoals()
+
+  return { pendingGoals }
+})
 
 app.post(
   '/goals',
@@ -21,7 +28,7 @@ app.post(
         desiredWeeklyFrequency: z.number().int().min(1).max(7),
       }),
     },
-  },  
+  },
   async request => {
     const { title, desiredWeeklyFrequency } = request.body
 
